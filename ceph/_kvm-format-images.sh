3645622e20769dfc98689a6eed2a21f0a418b3c2
jay-johnson <jay.p.h.johnson@gmail.com>
2019-03-06 19:15:40 +0000

    migrating from fdisk to using parted for setting up and formatting vm disks

--- ceph/_kvm-format-images.sh
+++ ceph/_kvm-format-images.sh
@@ -46,102 +46,100 @@ fi
 # https://www.cyberciti.biz/faq/how-to-add-disk-image-to-kvm-virtual-machine-with-virsh-command/
 nodes="master1.example.com master2.example.com master3.example.com"
 
-function format_as_xfs() {
-    for node in $nodes; do
-
-        ssh root@${node} "umount /dev/vdb1"
-        ssh root@${node} "sudo fdisk -l /dev/vdb"
-
-        # https://serverfault.com/questions/258152/fdisk-partition-in-single-line
-        anmt "Setting up ${node} partition for /dev/vdb"
-        ssh root@${node} "printf \"\nn\np\n1\n\n\nw\nq\n\" | sudo fdisk /dev/vdb"
-
-        # or manually:
-        # echo "# run on as root on the vm to format the disk in the vm:"
-        # echo "ssh root@${node}"
-        # echo "sudo fdisk /dev/vdb"
-        # echo "# enter commands:"
-        # echo "n"
-        # echo "p"
-        # echo "1"
-        # echo "w"
-
-        # ceph recommends xfs filesystems
-        # http://docs.ceph.com/docs/jewel/rados/configuration/filesystem-recommendations/
-        anmt "Formatting /dev/vdb1 as xfs"
-        ssh root@${node} "mkfs.xfs -f /dev/vdb1"
-
-        anmt "Removing previous mountpoint if exists: ${node}:/var/lib/ceph"
-        ssh root@${node} "rm -rf /var/lib/ceph >> /dev/null"
-
-        anmt "Creating /dev/vdb1 mountpoint: /var/lib/ceph"
-        ssh root@${node} "mkdir -p -m 775 /var/lib/ceph >> /dev/null"
-
-        # ssh root@${node} "umount /dev/vdb1"
-        ssh root@${node} "mount /dev/vdb1 /var/lib/ceph"
-
-        test_exists=$(ssh root@${node} "cat /etc/fstab | grep vdb1 | grep xfs | wc -l")
-        if [[ "${test_exists}" == "0" ]]; then
-            anmt "Adding /dev/vdb1 to /etc/fstab"
-            ssh root@${node} "echo \"/dev/vdb1 /var/lib/ceph  xfs     defaults    0 0\" >> /etc/fstab"
-        fi
-
-        anmt "Checking mounts"
-        ssh root@${node} "df -h | grep vdb1"
-    done
-}
-
-function umount_devices() {
-    anmt "--------------------------------"
-    anmt "Unmounting /dev/vdb devices on the cluster vms"
-    for node in $nodes; do
-        good " - ${node} umount /dev/vdb and /dev/vdb1"
-        ssh root@${node} "umount /dev/vdb1" >> /dev/null 2>&1
-        ssh root@${node} "umount /dev/vdb" >> /dev/null 2>&1
-    done
-}
-
-function partition_devices() {
-    anmt "--------------------------------"
-    anmt "Partitioning /dev/vdb devices on the cluster vms"
-    for node in $nodes; do
-        anmt " - ${node} sleeping umount on /dev/vdb"
-        sleep 5
-        anmt " - ${node} partitioning with fdisk /dev/vdb"
-        ssh root@${node} "printf \"d\n1\nd\nw\nq\n\" | sudo fdisk /dev/vdb"
-        sleep 2
-        good " - ${node} done partitioning with fdisk /dev/vdb"
-        anmt "--------------------------------"
-    done
-}
-
 function check_mounts() {
     anmt "--------------------------------"
     anmt "Checking mounted /dev paths on the cluster vms"
     for node in $nodes; do
-        good " - ${node} has: df -h | grep '/dev'"
-        ssh root@${node} "df -h | grep vdb"
-        ssh root@${node} "df -h | grep '/dev'"
-        good " - ${node} done partitioning with fdisk /dev/vdb"
+        good " - ${node} has: df -Th | grep 'vdb'"
+        ssh root@${node} "df -Th | grep vdb"
+        good " - ${node} done partitioning with parted /dev/vdb"
         anmt "--------------------------------"
     done
 }
 
-function check_on_fdisk_orphaned_processes() {
-    anmt "--------------------------------"
-    anmt "Checking for orphaned fdisk processes on the cluster vms"
+function delete_partitions_and_reformat_disks() {
     for node in $nodes; do
-        num_fdisk=$(ssh root@${node} "ps auwwx | grep fdisk | grep -v grep | grep '/dev/vdb' | wc -l")
-        if [[ "${num_fdisk}" != "0" ]]; then
-            err " - Error Detected ${node} has fdisk orphans: ${num_fdisk}"
-            exit 1
+
+        check_if_mounted=$(ssh root@${node} "df -Th /var/lib/ceph 2> /dev/null" | grep vdb1 | wc -l)
+        check_if_partitioned=$(ssh root@${node} "parted /dev/vdb print | grep -A 10 Number | grep -E 'MB|GB' | wc -l")
+
+        # Unmount device if mounted
+
+        if [[ "${check_if_mounted}" != "0" ]]; then
+            anmt "${node} - /dev/vdb1 umount"
+            ssh root@${node} "umount /dev/vdb1"
+            echo "${node} - sleeping to let umount finish"
+            sleep 2
+            check_if_mounted=$(ssh root@${node} "df -Th /var/lib/ceph 2> /dev/null" | grep vdb1 | wc -l)
+            if [[ "${check_if_mounted}" != "0" ]]; then
+                err "unable to umount /dev/vdb1 on node: ${node} - stopping"
+                exit 1
+            fi
         else
-            good " - ${node} has ${num_fdisk} fdisk orphans"
+            anmt "${node} - /dev/vdb1 is not mounted"
         fi
+
+        # Partition device if partitioned
+
+        if [[ "${check_if_partitioned}" != "0" ]]; then
+            anmt "${node} - already partitioned - deleting partitions: ${check_if_partitioned}"
+            ssh root@${node} "parted -s /dev/vdb rm 1 >> /dev/null 2>&1"
+            ssh root@${node} "parted -s /dev/vdb rm 2 >> /dev/null 2>&1"
+        else
+            good "no partitions: ${check_if_partitioned}"
+        fi
+
+        good "${node} - using parted to partition /dev/vdb"
+        # https://unix.stackexchange.com/questions/38164/create-partition-aligned-using-parted/49274#49274
+        ssh root@${node} "parted -a optimal /dev/vdb mkpart ceph 0% 100%"
+        sleep 2
+
+        anmt "${node} - checking /dev/vdb partitions"
+        check_if_partitioned=$(ssh root@${node} "parted /dev/vdb print | grep -A 10 Number | grep -E 'MB|GB' | wc -l")
+        if [[ "${check_if_partitioned}" != "1" ]]; then
+            err "Failed automated parted partitioning - please manually delete /dev/vdb partitions on ${node} with the commands and retry: "
+            ssh root@${node} "parted /dev/vdb print"
+            anmt "ssh root@${node}"
+            anmt "parted /dev/vdb"
+            exit 1
+        fi
+
+        # ceph recommends xfs filesystems
+        # http://docs.ceph.com/docs/jewel/rados/configuration/filesystem-recommendations/
+        anmt "${node} - formatting /dev/vdb1 as xfs"
+        ssh root@${node} "mkfs.xfs -f /dev/vdb1"
+
+        # anmt "${node} - formatting /dev/vdb2 as xfs"
+        # ssh root@${node} "mkfs.xfs -f /dev/vdb2"
+
+        anmt "${node} - removing previous mountpoint if exists: /var/lib/ceph"
+        ssh root@${node} "rm -rf /var/lib/ceph >> /dev/null"
+
+        anmt "${node} - creating /dev/vdb1 mountpoint: /var/lib/ceph"
+        ssh root@${node} "mkdir -p -m 775 /var/lib/ceph >> /dev/null"
+
+        # ssh root@${node} "umount /dev/vdb1"
+        anmt "${node} - mounting /dev/vdb1 to /var/lib/ceph"
+        ssh root@${node} "mount /dev/vdb1 /var/lib/ceph"
+
+        check_disk_filesystem=$(ssh root@${node} "df -Th /var/lib/ceph | grep vdb | grep xfs | wc -l")
+        if [[ "${check_disk_filesystem}" == "0" ]]; then
+            err "Failed to mount ${node}:/dev/vdb1 as xfs filesystem to /var/lib/ceph"
+            anmt "Please fix this node and retry:"
+            anmt "ssh root@${node}"
+        fi
+
+        test_exists=$(ssh root@${node} "cat /etc/fstab | grep vdb1 | grep xfs | wc -l")
+        if [[ "${test_exists}" == "0" ]]; then
+            anmt "adding /dev/vdb1 to /etc/fstab"
+            ssh root@${node} "echo \"/dev/vdb1 /var/lib/ceph  xfs     defaults    0 0\" >> /etc/fstab"
+        fi
+
+        anmt "${node} - checking mounts"
+        ssh root@${node} "df -Th | grep vdb1"
+        anmt "--------------------------------------------"
     done
 }
 
-umount_devices
-partition_devices
+delete_partitions_and_reformat_disks
 check_mounts
-check_on_fdisk_orphaned_processes
